741bdb85b0b0ae2088a349ea988a395a7951ee04
Altemir Almeida <altemir.git@gmail.com>
2020-10-14 11:33:00 +0100

    Installing dependencies

--- App.js
+++ App.js
@@ -2,12 +2,41 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          
+          tabBarIcon: ({ focused, color, size }) => { 
+            let iconName;
+            
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-home'
+                : 'ios-home';
+            } else if (route.name === 'My Places') {
+              iconName = focused ? 'ios-map' : 'ios-map';
+            }
+
+           
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        
+        }}
+      
+      >
+        <Tab.Screen name="Home" options={{}} component={HomeScreen} />
+        <Tab.Screen name="My Places" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
