ff622d83e4f3dc11dd4b3d07ee402e82c53eb910
Altemir Almeida <altemir.git@gmail.com>
2020-11-17 00:53:36 +0000

    Set hometab city and conversion input

--- App.js
+++ App.js
@@ -8,68 +8,95 @@ import HomeTab from './tabs/HomeTab';
 import MyPlacesTab from './tabs/MyPlacesTab';
 import WeatherTab from './tabs/WeatherTab';
 import * as Location from 'expo-location';
+import { openCageKey, openWeatherKey } from './keys.js';
 
-const Tab = createBottomTabNavigator(); 
-export default function App() {   
-  let [data, setData] = React.useState([]);
-  const [lat, setLatitude] = React.useState(0);
-  const [lng, setLongitude] = React.useState(0);
+const Tab = createBottomTabNavigator();
+export default function App() {
+  let [lat, setLatitude] = React.useState(null);
+  let [lng, setLongitude] = React.useState(null);
+  let [data, setData] = React.useState('');
+  let [currency, setCurrency] = React.useState('');
+  let [weather, setWeather] = React.useState('');
   const [errorMsg, setErrorMsg] = React.useState(null);
 
-  //Function to get gps location 
+  let iso_code = data && data.results[0].annotations.currency.iso_code;
+  let { rates } = currency && currency;
+  let conversion = rates && rates[Object.keys(rates)[0]];
+
+  //Function to get gps location
   React.useEffect(() => {
     (async () => {
       let { status } = await Location.requestPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
       }
-      let location = await Location.getCurrentPositionAsync({});
+      let location = await Location.getCurrentPositionAsync({ Accuracy: 6 });
       setLatitude(location.coords.latitude);
       setLongitude(location.coords.longitude);
     })();
   }, []);
 
-   //Function to fetch currency symbol
-   const fetchLocation = () =>{
-    React.useEffect( () => {
-      fetch(`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lng}&key=89f1a3d326ee4d2282442ef0a95d879a`,)
-        .then((response)=> response.json())
-        .then((json)=>setData(json))
-        .catch((err)=>console.log(err))
-     },[lat,lng]);
-   }
-console.log(data)
- fetchLocation();
+  //Function to fetch currency symbol
+  React.useEffect(() => {
+    fetch(
+      `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lng}&key=${openCageKey}`,
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json));
+    console.log(data);
+  }, [lng]);
+
+  React.useEffect(() => {
+    fetch(`https://api.exchangeratesapi.io/latest?base=USD&symbols=${iso_code}`)
+      .then((response) => response.json())
+      .then((json) => setCurrency(json));
+    console.log(iso_code);
+  }, [iso_code]);
+
+  React.useEffect(() => {
+    fetch(
+      `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${openWeatherKey}`,
+    )
+      .then((response) => response.json())
+      .then((json) => setWeather(json));
+    console.log(data);
+  }, [lng]);
+
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={({ route }) => ({
-          
-          tabBarIcon: ({ focused, color, size }) => { 
+          tabBarIcon: ({ focused, color, size }) => {
             let iconName;
             if (route.name === 'Home') {
-              iconName = focused
-                ? 'ios-home'
-                : 'ios-home';
+              iconName = focused ? 'ios-home' : 'ios-home';
             } else if (route.name === 'My Places') {
               iconName = focused ? 'ios-map' : 'ios-map';
             } else {
-              iconName = focused ? 'ios-partly-sunny':'ios-partly-sunny';
+              iconName = focused ? 'ios-partly-sunny' : 'ios-partly-sunny';
             }
 
-           
-            return <Ionicons name={iconName} size={size} color={color} />;
+            return (
+              <Ionicons
+                name={iconName}
+                size={size}
+                color={color}
+                rate={conversion}
+              />
+            );
           },
         })}
         tabBarOptions={{
           activeTintColor: 'tomato',
           inactiveTintColor: 'gray',
-          labelStyle: {fontSize:14}
+          labelStyle: { fontSize: 14 },
         }}
       >
-        <Tab.Screen name="Home"  children={() => <HomeTab geo={data} />} />
+        <Tab.Screen
+          name="Home"
+          children={() => <HomeTab geo={data} rate={conversion} />}
+        />
         <Tab.Screen name="Weather" component={WeatherTab} />
-
         <Tab.Screen name="My Places" component={MyPlacesTab} />
       </Tab.Navigator>
     </NavigationContainer>
--- tabs/HomeTab.js
+++ tabs/HomeTab.js
@@ -1,20 +1,33 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 const HomeTab = (props) => {
+  const { results } = props.geo;
+  const rate = props.rate;
+  const [total, setTotal] = React.useState('');
+  const [amount, setAmount] = React.useState('');
 
-  const {results } = props.geo;
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      
-      <Text>{results && results[0].components.county}</Text>
+      <Text>
+        Welcome to{' '}
+        {(results && results[0].components.county) ||
+          (results && results[0].components.city)}
+      </Text>
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: 70 }}
+        onChangeText={(amount) => setAmount(amount)}
+      />
+
+      <Button title="convert" onPress={() => setTotal(amount * rate)} />
+      <Text> {''}</Text>
+      <Text>
+        {total && total
+          ? total.toFixed(2) + results[0].annotations.currency.symbol
+          : 'Please, enter an amount (Number)'}
+      </Text>
     </View>
-  )
-}
+  );
+};
 
 export default HomeTab;
-
-
-
-
